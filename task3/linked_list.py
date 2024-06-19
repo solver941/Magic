f5bfa5af26da6facc8a70c63628dc90b53310d23
solver941 <marek.heckelmoser@seznam.cz>
2024-06-19 17:16:58 +0200

    fixed

--- task3/linked_list.py
+++ task3/linked_list.py
@@ -1,40 +1,44 @@
 class Node:
-    def __init__(self, data=None):
+    def __init__(self, prev_node=None, data=None, next_node=None):
         self.data = data
-        self.next_node = None
-        self.prev_node = None
+        self.next_node = next_node
+        self.prev_node = prev_node
 
 
 class DoublyLinkedList:
     def __init__(self):
         super().__init__()
-        self.head = None
-        self.tail = None
+        self.head: Node = None
+        self.tail: Node = None
         self.length = 0
 
     def append(self, data):
-        new_node = Node(data)
-        if self.length == 0:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            self.tail.next_node = new_node
-            new_node.prev_node = self.tail
-            self.tail = new_node
+        if len(self) == 0:
+            node = Node(None, data, None)
+            self.head = node
+            self.tail = node
+            self.length += 1
+            return
+
+        node = Node(self.tail, data, None)
+        self.tail.next_node = node
+        self.tail = node
         self.length += 1
 
     def insert(self, data, index):
-        index = self.check_index(index)
-
+        index = self.translate_index(index)
 
         if index == self.length:  # na konci
             self.append(data)
             return
 
-        if index == self.length:
-            self.append(data)
-            self.head.prev_node = None
-            node = Node()
+        if index == 0:  # na začátku
+            if len(self) == 0:
+                self.append(data)
+                return
+
+            node = Node(None, data, self.head)
+            self.head.prev_node = node
             self.head = node
             self.length += 1
             return
@@ -45,31 +49,14 @@ class DoublyLinkedList:
         prev_node.next_node = node
         self.length += 1
 
+    def __iter__(self):
+        ls = [self.head.data]
+        last = self.head
+        for _ in range(len(self) - 1):
+            ls.append(last.next_node.data)
+            last = last.next_node
 
-    def check_index(self, index):
-        if index < -self.length or index >= self.length:
-            raise IndexError("Index out of range")
-        if index < 0:
-            index += self.length
-        return index
-
-    def get_node(self, index):
-        current = self.head
-        for i in range(index):
-            current = current.next_node
-        return current
-
-
-    def __len__(self):
-        return self.length
-
-    def __getitem__(self, index):
-        return list(self)[index]
-        """
-        node = self.head
-        for i in range(index):
-            node = node.next_node
-        return node"""
+        return iter(ls)
 
     def __len__(self):
         return self.length
@@ -77,31 +64,39 @@ class DoublyLinkedList:
     def __getitem__(self, index):
         return list(self)[index]
 
-    def __setitem__(self, key, value):
-        pass
-
+    def __setitem__(self, index, data):
+        index = self.translate_index(index)
+        self.get_node(index).data = data
 
     def __delitem__(self, index):
-        index = self.check_index(index)
+        index = self.translate_index(index)
         node = self.get_node(index)
+
         prev = node.prev_node
         node.prev_node.next_node = node.next_node
         node.next_node.prev_node = prev
-        del node
         self.length -= 1
 
+    def translate_index(self, index):
+        if index > self.length or index < -len(self):
+            raise IndexError
 
-    def __iter__(self):
-        self.position = 0
-        return self
+        if index < 0:
+            index += len(self)
 
-    def __next__(self):
-        if self.position < len(self.data):
-            return_data = self.data[self.position]
-            self.position += 1
-            return return_data
-        else:
-            raise StopIteration
+        return index
+
+    def get_node(self, index) -> Node:
+        last = self.head
+        if index == 0:
+            return last
+
+        for i in range(len(self) - 1):
+            if i == index - 1:
+                return last.next_node
+            last = last.next_node
+
+        raise IndexError
 
 
 
